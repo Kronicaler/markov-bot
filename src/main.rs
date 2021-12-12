f645eaca4170f341f32d7bf327a486c880e8b9b0
Kroni <TheKroni@users.noreply.github.com>
2021-12-12 22:23:36 +0100

    Create data folders correctly

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,8 @@ const KRONI_ID: u64 = 594_772_815_283_093_524;
 
 #[tokio::main]
 async fn main() {
-    fs::create_dir("data/markov data").ok();
+    fs::create_dir("data").unwrap();
+    fs::create_dir("data/markov data").unwrap();
     dotenv::dotenv().expect("Failed to load .env file\nCreate a .env file in the same folder as the executable and type in the following without the braces or any whitespace:\n\nDISCORD_TOKEN={your discord token here}\nAPPLICATION_ID={your application id here}\n\n");
 
     select! {
