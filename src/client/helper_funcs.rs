015b31bc96f23a80563c780fa950d36ed0743aea
Kroni <TheKroni@users.noreply.github.com>
2022-01-09 21:58:28 +0100

    Refactor leave_unkown guilds

--- src/client/helper_funcs.rs
+++ src/client/helper_funcs.rs
@@ -32,28 +32,26 @@ pub async fn leave_unknown_guilds(ready: &Ready, ctx: &Context) {
         .await
         .expect("couldn't get application info")
         .owner;
-    for guild in &ready.guilds {
-        let bot_owner_in_guild = guild.id().member(&ctx.http, bot_owner.id).await;
+    for guild_status in &ready.guilds {
+        let guild_id = guild_status.id();
+
+        let bot_owner_in_guild = guild_id.member(&ctx.http, bot_owner.id).await;
 
         if let Err(serenity::Error::Http(_)) = bot_owner_in_guild {
-            println!("Couldn't find user in guild");
-            guild
-                .id()
-                .leave(&ctx.http)
-                .await
-                .expect("Couldn't leave guild");
+            println!("Couldn't find bot owner in guild");
+
+            guild_id.leave(&ctx.http).await.expect("Couldn't leave guild");
+
             println!(
                 "Left guild {} owned by {}",
-                guild
-                    .id()
+                guild_id
                     .name(&ctx.cache)
                     .await
                     .unwrap_or_else(|| "NO_NAME".to_owned()),
-                guild
-                    .id()
+                guild_id
                     .to_guild_cached(&ctx.cache)
                     .await
-                    .expect("Couldn't fetch server from cache")
+                    .expect("Couldn't fetch guild from cache")
                     .owner_id
                     .to_user(&ctx.http)
                     .await
