d140d2fe203c05bef54600531ed8cb52d7a6d92c
Kroni <TheKroni@users.noreply.github.com>
2022-11-23 18:21:56 +0100

    Dependency updates

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use serenity::model::id::GuildId;
 async fn main() {
     file_operations::create_data_folders();
 
-    dotenv::dotenv().expect(
+    dotenvy::dotenv().expect(
         "Failed to load .env file\n
         Create a .env file in the same folder as the executable and type in the following without the braces or any whitespace:\n\n
         DISCORD_TOKEN={your discord token here}\nAPPLICATION_ID={your application id here}\n\n");
