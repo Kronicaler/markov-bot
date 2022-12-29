24fb04da8bd75bbb754454ae32535d2a2c126f2c
Kroni <TheKroni@users.noreply.github.com>
2022-12-29 18:41:55 +0100

    Refactor create_data_folders

--- src/client/file_operations.rs
+++ src/client/file_operations.rs
@@ -11,10 +11,7 @@ pub fn create_file_if_missing<'a>(path: &'a str, contents: &str) -> Result<&'a s
 }
 
 pub fn create_data_folders() {
-    if !Path::new("data").exists() {
-        fs::create_dir("data").expect("Couldn't create directory data ");
-    };
     if !Path::new("data/markov data").exists() {
-        fs::create_dir("data/markov data").expect("Couldn't create directory data/markov data");
+        fs::create_dir_all("data/markov data").expect("Couldn't create directory data/markov data");
     };
 }
