b287788a0d9c18c1447a62d871bbded480041853
Kroni <TheKroni@users.noreply.github.com>
2023-09-03 19:05:48 +0200

    Containerization

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,7 @@ use serenity::model::id::GuildId;
 async fn main() {
     file_operations::create_data_folders();
 
-    dotenvy::dotenv().expect(
-        "Failed to load .env file\n
-        Create a .env file in the same folder as the executable and type in the following without the braces or any whitespace:\n\n
-        DISCORD_TOKEN={your discord token here}\nAPPLICATION_ID={your application id here}\n\n");
+    _ = dotenvy::dotenv();
 
     start().await;
 }
