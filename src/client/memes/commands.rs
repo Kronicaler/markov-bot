8da65b18bb239736ed1792ac4b9462093f9426f2
Dominik <68446074+Kronicaler@users.noreply.github.com>
2025-08-08 16:29:16 +0200

    Allow random memes to be sent in dms

--- src/client/memes/commands.rs
+++ src/client/memes/commands.rs
@@ -27,6 +27,7 @@ pub fn create_memes_commands() -> Vec<CreateCommand> {
                 .add_integration_type(InstallationContext::User)
                 .add_integration_type(InstallationContext::Guild)
                 .add_context(InteractionContext::Guild)
+                .add_context(InteractionContext::User)
         })
         .collect_vec();
 
