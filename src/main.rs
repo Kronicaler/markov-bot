ec97dcbcf899bd6958781a7b68dfa25ecfa23699
Dominik <68446074+Kronicaler@users.noreply.github.com>
2025-08-16 15:46:22 +0200

    Fix ytdlp update

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@
 mod client;
 mod logging;
 
+use std::process::Command;
+
 use chrono::Duration;
 use client::{file_operations, global_data, markov, start, tags, voice};
 use logging::setup_logging;
-use tokio::{process::Command, spawn, time::interval};
+use tokio::{spawn, time::interval};
 use tracing::{error, info, info_span};
 
 #[tokio::main(flavor = "multi_thread")]
@@ -26,22 +28,20 @@ async fn update_ytdlp_loop() {
     let mut interval = interval(Duration::days(1).to_std().unwrap());
     loop {
         interval.tick().await;
-        info_span!("updating_ytdlp")
-            .in_scope(
-                async || match Command::new("yt-dlp").args(["--update"]).output().await {
-                    Ok(o) => {
-                        let stdout = String::from_utf8(o.stdout).unwrap();
-                        let stderr = String::from_utf8(o.stderr).unwrap();
-                        if !stdout.is_empty() {
-                            info!(stdout);
-                        }
-                        if !stderr.is_empty() {
-                            error!(stderr);
-                        }
+        info_span!("updating_ytdlp").in_scope(|| {
+            match Command::new("yt-dlp").args(["--update"]).output() {
+                Ok(o) => {
+                    let stdout = String::from_utf8(o.stdout).unwrap();
+                    let stderr = String::from_utf8(o.stderr).unwrap();
+                    if !stdout.is_empty() {
+                        info!(stdout);
                     }
-                    Err(e) => error!(?e),
-                },
-            )
-            .await;
+                    if !stderr.is_empty() {
+                        error!(stderr);
+                    }
+                }
+                Err(e) => error!(?e),
+            }
+        });
     }
 }
