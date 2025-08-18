d83215243a312178e1950c732e7052ae859b6bb0
Dominik <68446074+Kronicaler@users.noreply.github.com>
2025-08-18 12:58:26 +0200

    Fix migration

--- migrations/20250818105128_save_files.sql
+++ migrations/20250818105128_save_files.sql
@@ -3,13 +3,14 @@
 CREATE TABLE IF NOT EXISTS file_hashes
 (
     hash            BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-	path            TEXT UNIQUE NOT NULL,
+	path            TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS file_categories
 (
 	category        TEXT NOT NULL,
-    file_hash       BIGINT NOT NULL FOREIGN KEY REFERENCES file_hashes(hash),
+    file_hash       BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY(file_hash) REFERENCES file_hashes(hash)
 );
 
 CREATE INDEX file_categories_category on file_categories(category);
