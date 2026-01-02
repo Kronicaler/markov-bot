acbc689702e5df614b128a2f354ce6c313c22718
Dominik <68446074+Kronicaler@users.noreply.github.com>
2026-01-02 07:03:00 +0100

    Finalize model

--- migrations/20250805215501_memes.sql
+++ migrations/20250805215501_memes.sql
@@ -1,25 +1,28 @@
--- track the last file of a category posted to a server
-CREATE TABLE IF NOT EXISTS meme_server_category_indexes (
+-- track the last file of a category posted to a discord server
+CREATE TABLE IF NOT EXISTS meme_server_categories (
     server_id BIGINT UNSIGNED NOT NULL,
-    category TEXT NOT NULL,
-    tag_index INT UNSIGNED NOT NULL
+    category_id INT NOT NULL,
+    file_id INT NOT NULL,
+    PRIMARY KEY(server_id, category),
+    FOREIGN KEY(category_id) REFERENCES meme_categories(id),
+    FOREIGN KEY(file_id) REFERENCES meme_files(id)
 );
 
-CREATE UNIQUE INDEX sfi on meme_server_folder_indexes(server_id, category);
-
-CREATE TABLE IF NOT EXISTS meme_file_hashes (
-    hash BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-    path TEXT UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS meme_files (
+    id SERIAL PRIMARY KEY,
+    folder TEXT UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS meme_file_categories (
     category_id INT NOT NULL,
-    file_hash BIGINT UNSIGNED NOT NULL,
-    FOREIGN KEY(file_hash) REFERENCES file_hashes(hash)
+    file_id INT NOT NULL,
+    PRIMARY KEY(category_id, file_id),
+    FOREIGN KEY(file_id) REFERENCES meme_files(id),
     FOREIGN KEY(category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE IF NOT EXISTS meme_categories (
-    id INT PRIMARY KEY NOT NULL,
-    category TEXT PRIMARY KEY NOT NULL
+    id SERIAL PRIMARY KEY,
+    category TEXT NOT NULL
 );
--- src/client/memes/doc.md
+++ src/client/memes/doc.md
@@ -3,7 +3,8 @@
 - allow a user upload an image, gif or video under 50MB with a command
 - allow the user to also execute this on a message with a link or uploaded file
 - when the command is executed open a modal with a file upload field (if the command wasn't executed on an existing message),
-  and let them input into a text field a list of categories/tags separated by spaces (max 10) (should the tags be predetermined?)
+  and let them input into a text field a list of categories/tags separated by spaces (max 10) (should the tags be predetermined? no)
 - the user then clicks the upload button to upload the meme to the bot
+- the meme gets saved to a folder with the sanitized name of the first tag
 - the user can later on then run a command /meme [tag] [ordered?] to upload a meme from that category
-- the ordered flag would send memes from that category from oldest to newest
+- the ordered flag would send memes from that category from oldest to newest for that server, default is false
